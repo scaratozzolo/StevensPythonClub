e0058ec6103c5138da46d8a344032ab284734eca
Scott Caratozzolo <scaratozzolo12@gmail.com>
2020-11-17 11:59:52 -0500

    starting script

---
+++ Week 11/stevens_register.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import os
+import time
+
+
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
